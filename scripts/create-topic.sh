c6acfe30c42c98c85b0e6dcb07f172279907adea
Francisco Pantoja <franciscopantojag98@gmail.com>
2022-01-06 19:57:25 -0500

    update create topic script

--- scripts/create-topic.sh
+++ scripts/create-topic.sh
@@ -1,6 +1,6 @@
 docker exec -it kafka /opt/bitnami/kafka/bin/kafka-topics.sh \
     --create \
-    --zookeeper zookeeper:2181 \
+    --bootstrap-server localhost:9092 \
     --replication-factor 1 \
     --partitions 1 \
     --topic test
